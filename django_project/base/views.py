20bc028a95d58016f93e9f7ff2c7574c52d53bbb
xevworks <alqaaf@lifematics.co.jp>
2024-05-02 11:10:07 +0900

    Created new templates folder

--- django_project/base/views.py
+++ django_project/base/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {"id":1, "name":"Lets learn python!"},
+    {"id":2, "name":"Design with me"},
+    {"id":3, "name":"Front-end developers"},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {"rooms":rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
